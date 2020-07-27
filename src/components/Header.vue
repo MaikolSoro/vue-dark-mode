338678761c3c36d36df306d8a3e80e7e19d3fbb3
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-27 06:04:02 -0600

    Css the  header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 <template>
 	<header>
 		<h1>{{ mode }} Mode</h1>
@@ -15,3 +16,37 @@ export default {
 	props:['mode']
 }
 </script>
+
+<style>
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    padding: 0px 25px;
+    height: 60px;
+    background: #C3C3C3;
+    /* transition:  background 0.3s ease-in-out; */
+  }
+  .dark Header{
+	background: #15202B;
+  }
+
+  header h1 {
+	font-size: 32px;
+	text-transform: uppercase;
+	font-weight: 900;
+  }
+  header nav {
+	display: flex;
+	height: 60px;
+  }
+  header nav .nav-link {
+	display: flex;
+	align-items: center;
+	padding: 0px 15px;
+  }
+  header nav .nav-link:hover {
+	background: #B0B0B0;
+  }
+</style>
